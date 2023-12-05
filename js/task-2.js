57996889a07d9e53efdcd66522a3d65cb69883cd
Roman Fedevych <fedevychr@gmail.com>
2023-12-05 22:43:58 +0200

    add3

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,103 @@
-function countProps(object) {
+// function countProps(object) {
 
-//   const keys = Object.keys(object);
-  
-    for (const key of Object.keys(object)) {
+//     for (const key of Object.keys(object)) {
 
-    } 
-  return Object.keys(object).length;
-}
+//     } 
+//   return Object.keys(object).length;
+// }
 
 
-console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
-console.log(countProps({ name: "Mango", age: 2 }));
-console.log(countProps({}));
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+// console.log(countProps({ name: "Mango", age: 2 }));
+// console.log(countProps({}));
+
+//////////////////////////////////////////////////
+
+// function greet(name) {
+//   return `Welcome ${name}!`;
+// }
+
+// // Викликаємо функцію greet і виводимо результат у консоль
+// console.log(greet("Mango")); // "Welcome Mango!"
+
+// // Виводимо функцію greet у консоль, не викликаючи її
+// console.log(greet); // ƒ greet() {return `Welcome ${name}!`}
+
+/////////////////////////////////////////////////
+
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+
+// // const result = makePizza();
+// // const pointer = makePizza;
+
+// console.log(makePizza());
+// console.log(makePizza);
+
+////////////////////////////////////////
+
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// function makeMessage(pizzaName, callbak) {
+//   return callbak(pizzaName);
+// }
+
+// console.log(makeMessage("Royal Grand", makePizza));
+// console.log(makeMessage("Ultracheese", deliverPizza));
+
+////////////////////////////////////////////////////////
+
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}`);
+// });
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}`);
+// });
+
+//////////////////////////////////////////////
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(function (item) {
+//     totalPrice += item;
+    
+//   });
+
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
+// console.log(calculateTotalPrice([164, 48, 291])); // 503
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176])); // 1116
+
+/////////////////////////////////////////////////////
+
+// function filterArray(numbers, value) {
+//     const array = [];
+//     numbers.forEach(function (number) {
+//         if (number > value) {
+//             array.push(number);
+//         }
+//     });
+//     return array;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
